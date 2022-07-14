6c7d97dcdfb03e3a6b4c174fa9ebfbeb5e809a0a
tnicacio <nicacio.t92@gmail.com>
2022-07-14 18:00:22 -0300

    feat - SalesCard component

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,12 @@
-import icon from './../../assets/img/notification-icon.svg'
-import './styles.css';
+import icon from "./../../assets/img/notification-icon.svg";
+import "./styles.css";
 
 function NotificationButton() {
-  return(
+  return (
     <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
+      <img src={icon} alt="Notificar" />
     </div>
-  )
+  );
 }
 
 export default NotificationButton;
