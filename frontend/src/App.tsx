1b9748a2b5bb2aa48b5972d4cc6fa4278a2d79c8
tnicacio <nicacio.t92@gmail.com>
2022-07-16 17:11:22 -0300

    feat - toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
+
 import Header from './components/Header';
 import MainContainer from './components/MainContainer';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <MainContainer />
     </>
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,32 @@
-import './styles.css';
+import "./styles.css";
 
-import icon from './../../assets/img/notification-icon.svg';
+import icon from "./../../assets/img/notification-icon.svg";
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { toast } from "react-toastify";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios
+    .post(`${BASE_URL}/sale/${id}/notification`)
+    .then(() =>
+      toast.success("SMS enviado com sucesso!", {
+        className: "toast-success",
+      })
+    )
+    .catch(() =>
+      toast.error("Erro ao enviar o SMS", {
+        className: "toast-error",
+      })
+    );
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
